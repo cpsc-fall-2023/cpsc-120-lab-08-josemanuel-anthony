694f63e82ad3f7982f0d0426be39a04803594a34
student <student@tuffix-vm.myguest.virtualbox.org>
2023-10-24 20:34:42 -0700

    fixed formatting

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -9,7 +9,7 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-  if (arguments.size() !=4) {
+  if (arguments.size() != 4) {
     std::cout << "error: you must supply three arguments ";
 
     return 1;
