0b5b4412b770a710e0efafc24ee8181808782ee4
anthonymmoore <AntMoore@csu.fullerton.edu>
2023-11-01 00:10:32 +0000

    done but i cant fix linter errors

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Anthony Moore
+// AntMoore@csu.fullerton.edu
+// @anthonymmoore
+// Partners: @JosemSandoval
 
 #include <iostream>
 #include <string>
@@ -7,25 +10,30 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
+  if (arguments.size() <= 1) {
+    std::cerr << "error: you must supply at least one number" << std::endl;
+    return 1;
+  }
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
+  double sum = 0.0;
+  int count = 0;
 
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
+  for (int i = 1; i < arguments.size(); i++) {
+    try {
+      double value = std::stod(arguments[i]);
+      sum += value;
+      count++;
+    } catch (std::invalid_argument& e) {
+      std::cerr << "error: invalid input at argument " << i << std::endl;
+      return 1;
+    } catch (std::out_of_range& e) {
+      std::cerr << "error: argument " << i << "is out of range" << std::endl;
+      return 1;
+    }
+  }
 
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
+  double average = sum / count;
 
+  std::cout << "average = " << average << std::endl;
   return 0;
 }
